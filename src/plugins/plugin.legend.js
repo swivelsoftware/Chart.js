7eac43d3efb3de50182bea71502ab6319a697b95
Kennys Ng <kennysng@hotmail.com.hk>
2018-12-11 09:44:34 +0800

    test

--- src/plugins/plugin.legend.js
+++ src/plugins/plugin.legend.js
@@ -393,7 +393,7 @@ var Legend = Element.extend({
 					var radius = fontSize * Math.sqrt(5) / 5;
 					var centerX = x + boxWidth / 2;
 					var centerY = y + fontSize / 2;
-					ctx.lineWidth = 0.2
+					console.log('ctx.lineWidth', ctx.lineWidth)
 					helpers.canvas.drawPoint(ctx, legendItem.pointStyle, radius, centerX, centerY);
 				} else {
 					// Draw box as legend symbol
