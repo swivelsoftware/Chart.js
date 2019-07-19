730f1d5b36ad1d9a0d2846f9749011372012d07c
Kennys Ng <kennysng@hotmail.com.hk>
2019-07-19 14:30:17 +0800

    test

--- src/core/core.tooltip.js
+++ src/core/core.tooltip.js
@@ -776,7 +776,7 @@ var exports = Element.extend({
 
 	drawBody: function(pt, vm, ctx) {
 		var me = this;
-		console.log('core.tooltip.js', 'drawBody', pt, vm, ctx);
+		var ci = me.chart;
 		var bodyFontSize = vm.bodyFontSize;
 		var bodySpacing = vm.bodySpacing;
 		var bodyAlign = vm._bodyAlign;
@@ -817,21 +817,35 @@ var exports = Element.extend({
 
 			lines = bodyItem.lines;
 			for (j = 0, jlen = lines.length; j < jlen; ++j) {
+				var meta = ci.getDatasetMeta(i);
+
 				// Draw Legend-like boxes if needed
 				if (drawColorBoxes) {
-					// Fill a white rect so that colours merge nicely if the opacity is < 1
-					ctx.fillStyle = vm.legendColorBackground;
-					ctx.fillRect(colorX, pt.y, bodyFontSize, bodyFontSize);
+					if (meta.type === 'line') {
+						// Draw line as legend symbol
+						ctx.strokeRect(x, y + fontSize / 2, boxWidth, 0);
 
-					// Border
-					ctx.lineWidth = 1;
-					ctx.strokeStyle = labelColors.borderColor;
-					ctx.strokeRect(colorX, pt.y, bodyFontSize, bodyFontSize);
+						// Draw point at center
+						var radius = fontSize * Math.sqrt(5) / 5;
+						var centerX = x + boxWidth / 2;
+						var centerY = y + fontSize / 2;
+						ctx.lineWidth *= Math.SQRT2 / 2;
+						helpers.canvas.drawPoint(ctx, legendItem.pointStyle, radius, centerX, centerY, legendItem.rotation);
+					} else {
+						// Fill a white rect so that colours merge nicely if the opacity is < 1
+						ctx.fillStyle = vm.legendColorBackground;
+						ctx.fillRect(colorX, pt.y, bodyFontSize, bodyFontSize);
 
-					// Inner square
-					ctx.fillStyle = labelColors.backgroundColor;
-					ctx.fillRect(colorX + 1, pt.y + 1, bodyFontSize - 2, bodyFontSize - 2);
-					ctx.fillStyle = textColor;
+						// Border
+						ctx.lineWidth = 1;
+						ctx.strokeStyle = labelColors.borderColor;
+						ctx.strokeRect(colorX, pt.y, bodyFontSize, bodyFontSize);
+
+						// Inner square
+						ctx.fillStyle = labelColors.backgroundColor;
+						ctx.fillRect(colorX + 1, pt.y + 1, bodyFontSize - 2, bodyFontSize - 2);
+						ctx.fillStyle = textColor;
+					}
 				}
 
 				fillLineOfText(lines[j]);
